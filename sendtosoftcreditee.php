65bc0634602c8532073767fd8f561c29c0c0f7ea
Brienne Kordis <brienne@megaphonetech.com>
2023-07-07 10:55:05 -0400

    civix upgrade

--- sendtosoftcreditee.php
+++ sendtosoftcreditee.php
@@ -13,39 +13,12 @@ function sendtosoftcreditee_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function sendtosoftcreditee_civicrm_xmlMenu(&$files) {
-  _sendtosoftcreditee_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
  */
 function sendtosoftcreditee_civicrm_install() {
   _sendtosoftcreditee_civix_civicrm_install();
-}
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function sendtosoftcreditee_civicrm_postInstall() {
-  _sendtosoftcreditee_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function sendtosoftcreditee_civicrm_uninstall() {
-  _sendtosoftcreditee_civix_civicrm_uninstall();
 }
 
 /**
@@ -57,90 +30,6 @@ function sendtosoftcreditee_civicrm_enable() {
   _sendtosoftcreditee_civix_civicrm_enable();
 }
 
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function sendtosoftcreditee_civicrm_disable() {
-  _sendtosoftcreditee_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function sendtosoftcreditee_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _sendtosoftcreditee_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
- */
-function sendtosoftcreditee_civicrm_managed(&$entities) {
-  _sendtosoftcreditee_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function sendtosoftcreditee_civicrm_caseTypes(&$caseTypes) {
-  _sendtosoftcreditee_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function sendtosoftcreditee_civicrm_angularModules(&$angularModules) {
-  _sendtosoftcreditee_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function sendtosoftcreditee_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _sendtosoftcreditee_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function sendtosoftcreditee_civicrm_entityTypes(&$entityTypes) {
-  _sendtosoftcreditee_civix_civicrm_entityTypes($entityTypes);
-}
-
-/**
- * Implements hook_civicrm_thems().
- */
-function sendtosoftcreditee_civicrm_themes(&$themes) {
-  _sendtosoftcreditee_civix_civicrm_themes($themes);
-}
-
 // --- Functions below this ship commented out. Uncomment as required. ---
 
 /**
@@ -148,9 +37,8 @@ function sendtosoftcreditee_civicrm_themes(&$themes) {
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
  *
-function sendtosoftcreditee_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
